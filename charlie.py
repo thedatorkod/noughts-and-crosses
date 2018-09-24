35dfae38194cc20ef2364f0965b6de676afc9017
thedatorkod <jadonbarnes24@gmail.com>
2018-09-24 23:58:44 +0100

    Noughts and crosses

---
+++ charlie.py
@@ -0,0 +1,12 @@
+
+num1= input(print("enter number "))
+sign = input (print("enter sign"))
+num2= input(print("enter number "))
+
+if sign == "plus":
+    Result =  int(num1) + int(num2)
+    print(int(Result))
+
+elif sign == "minus":
+    Result =  int(num1) - int(num2)
+    print(int(Result))
